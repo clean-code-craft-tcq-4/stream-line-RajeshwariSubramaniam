c4e1cf58e3f8bff8efa856c509fbdb56273bce57
SruthiAccount <88025989+SruthiAccount@users.noreply.github.com>
2022-11-10 10:27:43 +0530

    Update ReceiverTest.java

--- receiver/ReceiverTest.java
+++ receiver/ReceiverTest.java
@@ -7,10 +7,6 @@ import java.util.List;
 
 import org.junit.Test;
 
-
-
-
-
 public class ReceiverTest {
 
 @Test
@@ -18,6 +14,16 @@ public class ReceiverTest {
 		HashMap<String,ArrayList<Float>> paramData=Receiver.readData();
 		assertNotNull(paramData);
 	}
+@Test
+public void queryTestDataHasCorrectParamCount() {
+	HashMap<String,ArrayList<Float>> paramData=Receiver.readData();
+	assertEquals(paramData.keySet().size(),2);
+}
+@Test
+public void queryTestDataFailsForIncorrectParamCount() {
+	HashMap<String,ArrayList<Float>> paramData=Receiver.readData();
+	assertNotEquals(paramData.keySet().size(),3);
+}
 
 	
 
